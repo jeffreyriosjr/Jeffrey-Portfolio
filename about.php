db1c6a99b3111cf227637c87772cf196a882b1a2
jeffrey rios jr <jeffriosjr@gmail.com>
2023-02-21 22:02:04 -0800

    adding new pages

---
+++ about.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: About Page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package jeffrey_alexander_rios_jr
+ */
+
+get_header();
+?>
---
+++ home.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Home Page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package jeffrey_alexander_rios_jr
+ */
+
+get_header();
+?>
