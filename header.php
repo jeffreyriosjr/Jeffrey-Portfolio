a8dddd648faaceb2bdf0bf633f0511a012d8b56a
jeffrey rios jr <jeffriosjr@gmail.com>
2023-02-27 09:47:16 -0800

    updated portfolio

--- header.php
+++ header.php
@@ -11,28 +11,25 @@
 
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes();?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset');?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
-	<?php wp_head(); ?>
+	<?php wp_head();?>
 </head>
 
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-
-
-		<!-- <nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'jarj' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav>#site-navigation -->
-	</header><!-- #masthead -->
+<body <?php body_class();?>>
+<?php wp_body_open();?>
+<div class="circle container p-0">
+	<span></span>
+	<span></span>
+	<span></span>
+	<span></span>
+</div>
+<div class="logo-box">
+<img src="http://jeffrey-alexander-rios-jr.local/wp-content/uploads/2023/02/Beige-Black-Elegant-Aesthetic-Modern-Font-Initials-Logo-1.png" class="logo">
+</div>
