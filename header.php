e39b3d4eee19fae7ae79117bb37c9850efcb0b28
jeffrey rios jr <jeffriosjr@gmail.com>
2023-02-27 14:13:21 -0800

    ref(header-footer):updated

--- header.php
+++ header.php
@@ -24,6 +24,7 @@
 
 <body <?php body_class();?>>
 <?php wp_body_open();?>
+<div class="header container-fluid">
 <div class="circle container p-0">
 	<span></span>
 	<span></span>
@@ -32,4 +33,5 @@
 </div>
 <div class="logo-box">
 <img src="http://jeffrey-alexander-rios-jr.local/wp-content/uploads/2023/02/Beige-Black-Elegant-Aesthetic-Modern-Font-Initials-Logo-1.png" class="logo">
+</div>
 </div>
