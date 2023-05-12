d18d4d11a5eeea28bd1bdc9fb311eee59e69c3c6
jeffrey rios jr <jeffriosjr@gmail.com>
2023-05-12 00:35:16 -0700

    updated new portfolio

--- header.php
+++ header.php
@@ -34,15 +34,4 @@
 
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
-	<div class="header container-fluid">
-		<div class="circle container p-0">
-			<span></span>
-			<span></span>
-			<span></span>
-			<span></span>
-		</div>
-		<div class="logo-box">
-			<img src="http://jeffrey-alexander-rios-jr.local/wp-content/uploads/2023/02/Beige-Black-Elegant-Aesthetic-Modern-Font-Initials-Logo-1.png"
-				class="logo">
-		</div>
-	</div>
+	
