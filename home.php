ad0e741d0e64366cfe56b8853dc9d10f9fb1e793
jeffrey rios jr <jeffriosjr@gmail.com>
2023-09-10 22:56:40 -0700

    testing

--- home.php
+++ home.php
@@ -9,3 +9,24 @@
 
 get_header();
 ?>
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NFL Pick'em Game</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>NFL Pick'em Game</h1>
+    </header>
+    <main>
+        <!-- Game content will go here -->
+    </main>
+    <footer>
+        <p>&copy; 2023 YourWebsiteName</p>
+    </footer>
+    <script src="script.js"></script>
+</body>
+
